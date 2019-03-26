bb37096273107eed4b086936e3b53b8a529bfbe6
Egill Anton Hlöðversson <egillanton@live.com>
2019-03-26 20:00:29 +0000

    run.sh fixed

--- s5/run.sh
+++ s5/run.sh
@@ -8,10 +8,7 @@ train_cmd="utils/run.pl"
 decode_cmd="utils/run.pl"
 
 if [ ! -d waves_yesno ]; then
-  wget http://www.openslr.org/resources/1/waves_yesno.tar.gz || exit 1;
-  # was:
-  # wget http://sourceforge.net/projects/kaldi/files/waves_yesno.tar.gz || exit 1;
-  tar -xvzf waves_yesno.tar.gz || exit 1;
+  tar -xf waves_yesno.tar.xz || exit 1;
 fi
 
 train_yesno=train_yesno
